e6aa00cc6dc6b9b795e0215fd48c71cdc01e9b9d
juanmates <jmates2556@ceuandalucia.es>
2025-10-16 22:37:32 +0200

    x

--- MÓDULO PROFESIONAL OPTATIVO/API REST/WORKSPACE/ejercicio12/src/main/java/com/ejercicios/controllers/LibroResponseController.java
+++ MÓDULO PROFESIONAL OPTATIVO/API REST/WORKSPACE/ejercicio12/src/main/java/com/ejercicios/controllers/LibroResponseController.java
@@ -1,10 +1,20 @@
 package com.ejercicios.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ejercicios.modelos.Libro;
 
 @RestController
 @RequestMapping("/libros")
 public class LibroResponseController {
+	
+	private List<Libro> libros = new ArrayList<>();
+	
+	
+			
 
 }
